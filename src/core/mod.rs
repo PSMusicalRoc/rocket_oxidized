014ecc2c74f937a4bdbd57efbd73cd50702b7319
DumbAsARoc <timbishop514@gmail.com>
2024-09-17 10:25:10 -0400

    documented RocketApplicationBuilder

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,22 @@
+//! # Core Library Functions
+//! 
+//! This file is the root of a solid chunk of the engine,
+//! including:
+//! - The app builder itself and startup procedures
+//! - Windowing and backends
+//! - Error system
+//! - Event system
+//! - App update (and @todo fixedupdate?) loops
+//! 
+//! To start, include `rocket_oxidized::core::RocketApplicationBuilder`
+//! in your project. This struct acts as the creation point
+//! for any apps you make with the engine. You are able to
+//! provide custom loop code, application name, and
+//! other (@todo!) TBD features. To see more, see the
+//! page for the [RocketApplicationBuilder] struct itself.
+//! 
+//! 
+
 use std::time::{Duration, Instant};
 
 use app::QueryApplication;
@@ -9,17 +28,48 @@ pub mod error;
 pub mod events;
 pub mod startup;
 
+/// The current version of the Rocket Engine! Used
+/// primarily for initialization.
 pub const ROCKET_VERSION: &str = "v0.0.0 dev";
 
+/// The function signature for the "update"
+/// function passed into [RocketApplicationBuilder].
 pub type RocketMainloopType = fn(f32);
 
+/// This struct dictates the creation of a [RocketApplication].
+/// It serves to allow the user to customize most everything
+/// about the application, including its name, loop functions,
+/// and more (@todo!)
+/// 
+/// Here's a very basic example of using this builder in your
+/// code:
+/// ```
+/// use rocket_oxidized::core::RocketApplicationBuilder;
+/// use rocket_oxidized::core::app::QueryApplication;
+/// 
+/// fn main() {
+///     let mut app = RocketApplicationBuilder::new()
+///         .set_application_name(format!("Hello world!"))
+///         .set_mainloop(|f: f32| {
+///             use std::process::exit;
+///             exit(0);
+///     }).build();
+///     app.run_application();
+/// }
+/// ```
 pub struct RocketApplicationBuilder {
+    /// The name of the built application
     application_name: String,
+
+    /// The function that will be called in the
+    /// 'update' thread (aka, not fixed deltatime)
     mainloop: RocketMainloopType
 }
 
 impl RocketApplicationBuilder {
 
+    /// Creates a new Builder. Call this in your
+    /// `main.rs` file to start creating your application.
     pub fn new() -> RocketApplicationBuilder {
         RocketApplicationBuilder {
             application_name: format!(""),
@@ -27,16 +77,33 @@ impl RocketApplicationBuilder {
         }
     }
 
+    /// Sets the application name for the created
+    /// application. This is used primarily in the
+    /// window's titlebar.
+    /// 
+    /// # Parameters
+    /// - `name`: The specific name of the application.
     pub fn set_application_name(&mut self, name: String) -> &mut Self {
         self.application_name = name.clone();
         self
     }
 
+    /// Provides the "update" loop for the created
+    /// application. This loop is deltatime-dependent,
+    /// and runs as fast as possible. Good for input
+    /// grabbing, less good for physics and rendering.
+    /// 
+    /// # Parameters
+    /// - `mainloop`: A function (either lambda or otherwise)
+    /// that matches the [RocketMainloopType] signature.
     pub fn set_mainloop(&mut self, mainloop: RocketMainloopType) -> &mut Self {
         self.mainloop = mainloop;
         self
     }
 
+    /// Builds and returns a [RocketApplication] based
+    /// on the builder inputs. From here, the user can
+    /// run their program.
     pub fn build(&mut self) -> RocketApplication {
         RocketApplication {
             application_name: self.application_name.clone(),
