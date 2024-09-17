08b3feed9659ae04d3c17a7ea3713d1ba0a3db30
DumbAsARoc <timbishop514@gmail.com>
2024-09-17 17:32:08 -0400

    basic documentation for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! # Welcome to the Rocket Engine!
+//! 
+//! This is a passion project written in Rust
+//! by Tim Bishop. Rocket is intended to be a
+//! small engine focused on making graphical
+//! applications and games. Its focus is on
+//! customizability (such that you can use this
+//! project for more than just one thing!) and
+//! ease of use.
+
 #![warn(missing_docs)]
 
 pub mod core;
