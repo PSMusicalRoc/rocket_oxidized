61a65735159f9dd2147d59861ad81d838465d983
DumbAsARoc <timbishop514@gmail.com>
2024-09-17 09:35:05 -0400

    removed commented out code

--- src/core/app/appstate.rs
+++ src/core/app/appstate.rs
@@ -1,12 +1,6 @@
 use lazy_static::lazy_static;
 use std::sync::RwLock;
 
-// pub trait AppState {
-
-//     fn is_app_still_running(&self) -> bool;
-//     fn quit_application(&mut self);
-// }
-
 trait RocketResettable {
 
     fn reset(&mut self);
@@ -30,18 +24,6 @@ impl RocketResettable for AppStateContainer {
         self.is_running = true;
     }
 }
-
-
-// impl AppState for AppStateContainer {
-
-//     fn is_app_still_running(&self) -> bool {
-//         self.is_running
-//     }
-
-//     fn quit_application(&mut self) {
-//         self.is_running = false;
-//     }
-// }
 
 unsafe impl Send for AppStateContainer {}
 unsafe impl Sync for AppStateContainer {}
